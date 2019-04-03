d0b9df05f2cc2f7007327f31d6fb9a71b488ac2e
Thomas Edwards <thce28@gmail.com>
2019-04-03 16:33:46 +0100

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json)); 
 }
 
 function renderBooks(json) {
@@ -13,3 +16,7 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+// function getPosts() {
+//   fetchBooks()
+// }
